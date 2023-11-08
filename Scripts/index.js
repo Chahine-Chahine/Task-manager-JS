c6fa31336542b154c185ba1b4e78cbd4ddf4978e
Chahine-Chahine <chahinesquare@hotmail.com>
2023-11-08 15:39:32 +0200

    added  the edit functionality

--- Scripts/index.js
+++ Scripts/index.js
@@ -2,41 +2,49 @@
 let add_button = document.getElementById("add_button");
 let button_form = document.querySelector("#button_form");
 
-
 // Add a click event listener to the button
 add_button.addEventListener("click", addTask);
 
-// 
 function addTask(event) {
-    // Select the placeholder value
-    let task_text = button_form.querySelector("input[type='text']").value;
-    // Prevent the form from submitting
-    event.preventDefault();
-    if (task_text !== "") {
+
+  // Select the placeholder value
+  let task_text = button_form.querySelector("input[type='text']").value;
+
+  // Prevent the form from submitting
+  event.preventDefault();
+  if (task_text !== "") {
+
     // Creating a div
-    let new_item = document.createElement("div"); 
+    let new_item = document.createElement("div");
+    
     // Adding class "item"
-    new_item.classList.add("item"); 
+    new_item.classList.add("item");
     // adding html code to the innerHtml of new_item
-    new_item.innerHTML += ` <input type="checkbox"><p>${task_text}</p> <img class="delete-task" src="./Assets/Images/trash-icon.svg" alt="Delete">`;
+    new_item.innerHTML += ` <input type="checkbox"><p>${task_text}</p><img class="edit_task" src="./Assets/Images/edit_icon.svg" alt="edit">
+                                                                 <img class="delete-task" src="./Assets/Images/trash-icon.svg" alt="Delete">`;
     // Select the container we will append to
-    let task_container = document.getElementById("task_container")
-    task_container.appendChild(new_item)
+    let task_container = document.getElementById("task_container");
+    task_container.appendChild(new_item);
     // Clear the input field for the next task
     button_form.querySelector("input[type='text']").value = "";
 
     // Select the image of trash can
     let deleteButton = new_item.querySelector(".delete-task");
     // Delete function on eventListener click
-    deleteButton.addEventListener("click", function() {
-        // Remove the child that is appended
-        task_container.removeChild(new_item);
-    
-    
-
+    deleteButton.addEventListener("click", function () {
+      // Remove the child that is appended
+      task_container.removeChild(new_item);
     });
-}else alert("You did not provide any task to add")
+    // Select the edit icon
+    let edit_task = new_item.querySelector(".edit_task");
+    console.log(edit_task);
+    // function of editing
+    edit_task.addEventListener("click", function () {
+      console.log("hey");
+      let paragraph = new_item.getElementsByTagName("p");
+      paragraph.textContent = prompt("Edit the task:", paragraph.textContent);
+      new_item.innerHTML = `<input type="checkbox"><p>${paragraph.textContent}</p><img class="edit_task" src="./Assets/Images/edit_icon.svg" alt="edit">
+                                                                 <img class="delete-task" src="./Assets/Images/trash-icon.svg" alt="Delete">`;
+    });
+  } else alert("You did not provide any task to add");
 }
-
-
-
