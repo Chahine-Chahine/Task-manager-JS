1c2b5a048ee253d203bfb5242e116e1368745a96
Chahine-Chahine <chahinesquare@hotmail.com>
2023-11-08 23:39:18 +0200

    implemented the logic for filters

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,14 +1,13 @@
 // Select the button and button form
 let add_button = document.getElementById("add_button");
 let button_form = document.querySelector("#button_form");
+let task_container = document.getElementById("task_container");
 
 // Add a click event listener to the button
 add_button.addEventListener("click", addTask);
 
-// Arrays to store checked and unchecked tasks
+// tasks array
 let tasks = [];
-let checkedTasks = [];
-let uncheckedTasks = [];
 
 function addTask(event) {
   // Select the placeholder value
@@ -23,15 +22,15 @@ function addTask(event) {
     // Adding class "item"
     new_item.classList.add("item");
 
-    // adding html code to the innerHtml of new_item
+    // adding HTML code to the innerHTML of new_item
     new_item.innerHTML += ` <input type="checkbox"><p>${task_text}</p><img class="edit_task" src="./Assets/Images/edit_icon.svg" alt="edit">
                                                                  <img class="delete-task" src="./Assets/Images/trash-icon.svg" alt="Delete">`;
-    // Select the container we will append to
-    let task_container = document.getElementById("task_container");
-    task_container.appendChild(new_item);
 
-    // Clear the input field for the next task
-    button_form.querySelector("input[type='text']").value = "";
+    // Select the checkbox within the new_item element
+    let checkbox = new_item.querySelector("input[type='checkbox");
+
+    // Store the new item in the tasks array
+    tasks.push(new_item);
 
     // Select the image of trash can
     let deleteButton = new_item.querySelector(".delete-task");
@@ -40,23 +39,67 @@ function addTask(event) {
     deleteButton.addEventListener("click", function () {
       // Remove the child that is appended
       task_container.removeChild(new_item);
+      // Remove the task from the tasks array
+      tasks.splice(tasks.indexOf(new_item), 1);
     });
 
     // Select the edit icon
     let edit_task = new_item.querySelector(".edit_task");
 
-    // function of editing
+    // Function for editing
     edit_task.addEventListener("click", function () {
       // Select the paragraph to edit
-      let paragraph = new_item.getElementsByTagName("p")[0];
+      let paragraph = new_item.querySelector("p");
       // Get new value
       let newText = prompt("Edit the task:", paragraph.textContent);
-      // Assign the new value
-      paragraph.textContent = newText;
+      if (newText !== null) {
+        // Assign the new value
+        paragraph.textContent = newText;
+      }
     });
 
-    
+    // Add the new_item to the task container
+    task_container.appendChild(new_item);
 
+    // Clear the input field for the next task
+    button_form.querySelector("input[type='text']").value = "";
 
-  } else alert("You did not provide any task to add");
+    // Check the checkbox and filter the displayed tasks
+    checkbox.addEventListener("change", function () {
+      filterTasks();
+    });
+
+    // Initialize the checkbox's checked status
+    checkbox.checked = false;
+
+    filterTasks();
+  } else {
+    alert("You did not provide any task to add");
+  }
 }
+
+// Function to filter tasks based on the checkboxes
+function filterTasks() {
+  let checkboxStatus = document.querySelector(".completed").classList.contains("selected") ? true : false;
+
+  tasks.forEach((task) => {
+    let checkbox = task.querySelector("input[type='checkbox']");
+    let isCompleted = checkbox.checked;
+    if ((isCompleted && checkboxStatus) || (!isCompleted && !checkboxStatus)) {
+      task.style.display = "flex";
+    } else {
+      task.style.display = "none";
+    }
+  });
+}
+
+// Add click event listeners to the filter options (All, Completed, Incomplete)
+document.querySelectorAll(".heading h2").forEach((filter) => {
+  filter.addEventListener("click", function () {
+    document.querySelectorAll(".heading h2").forEach((h2) => {
+      h2.classList.remove("selected");
+    });
+    this.classList.add("selected");
+    filterTasks();
+  });
+});
