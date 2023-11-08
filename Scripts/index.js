d473f1fe32edf58a520e0d859c5e938060297545
Chahine-Chahine <chahinesquare@hotmail.com>
2023-11-08 21:22:05 +0200

    added comments and refactored code

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,4 +1,3 @@
-
 // Select the button and button form
 let add_button = document.getElementById("add_button");
 let button_form = document.querySelector("#button_form");
@@ -7,21 +6,18 @@ let button_form = document.querySelector("#button_form");
 add_button.addEventListener("click", addTask);
 
 function addTask(event) {
-
   // Select the placeholder value
   let task_text = button_form.querySelector("input[type='text']").value;
-
 
   // Prevent the form from submitting
   event.preventDefault();
   if (task_text !== "") {
-
     // Creating a div
     let new_item = document.createElement("div");
 
     // Adding class "item"
     new_item.classList.add("item");
-    
+
     // adding html code to the innerHtml of new_item
     new_item.innerHTML += ` <input type="checkbox"><p>${task_text}</p><img class="edit_task" src="./Assets/Images/edit_icon.svg" alt="edit">
                                                                  <img class="delete-task" src="./Assets/Images/trash-icon.svg" alt="Delete">`;
@@ -32,13 +28,11 @@ function addTask(event) {
     // Clear the input field for the next task
     button_form.querySelector("input[type='text']").value = "";
 
-
     // Select the image of trash can
     let deleteButton = new_item.querySelector(".delete-task");
 
     // Delete function on eventListener click
     deleteButton.addEventListener("click", function () {
-
       // Remove the child that is appended
       task_container.removeChild(new_item);
     });
@@ -48,11 +42,12 @@ function addTask(event) {
 
     // function of editing
     edit_task.addEventListener("click", function () {
-      console.log("hey");
+      // Select the paragraph to edit
       let paragraph = new_item.getElementsByTagName("p")[0];
+      // Get new value
       let newText = prompt("Edit the task:", paragraph.textContent);
-        paragraph.textContent = newText;
+      // Assign the new value
+      paragraph.textContent = newText;
     });
-
   } else alert("You did not provide any task to add");
 }
